84865e774e412c4c64b1697fea2419b9b24b1a84
bilzar <bilal.zatboua@live.fr>
2023-03-20 15:20:40 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry piont
+ * Description: C program that prints base 10 digit
+ * Return: always 0 (Success)
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
