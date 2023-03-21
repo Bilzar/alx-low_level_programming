da457434b101f094b12eb937aeacc3518ab4420b
bilzar <bilal.zatboua@live.fr>
2023-03-20 17:21:07 -0700

    10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry piont
+ * Description: C program that prints combi of 2  digit
+ * Return: always 0 (Success)
+*/
+int main(void)
+{
+	int n1 = 0, n2;
+
+	while (n1 <= 9)
+	{
+		n2 = 0;
+		while (n2 <= 9)
+		{
+			if (n1 != n2 && n1 < n2)
+			{
+				putchar(n1 + 48);
+				putchar(n2 + 48);
+
+				if (n1 + n2 != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			n2++;
+		}
+		n1++;
+	}
+	putchar('\n');
+	return (0);
+}
