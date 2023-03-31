d884c8215989dd4f138b66add007fed766ec4025
bilzar <bilal.zatboua@live.fr>
2023-03-31 16:58:36 -0700

    4.1mpa&s

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -13,10 +13,11 @@ void reverse_array(int *a, int n)
 {
 	int i, t;
 
-	for (i = n - 1; i >= n / 2; i--)
+	for (i = 0; i < n; i++)
 	{
-	t = a[n - 1 - i];
-	a[n - 1 - i] = a[i];
-	a[i] = t;
+		n--;
+		t = a[i];
+		a[i] = a[n];
+		a[n] = t;
 	}
 }
