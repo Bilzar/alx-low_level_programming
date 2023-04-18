d90665151483cc438be456b869719c67d56c8aad
bilzar <bilal.zatboua@live.fr>
2023-04-17 19:46:40 -0700

    struc0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's struc
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ * Description: poppy
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
